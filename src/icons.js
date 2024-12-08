1dcc74c87098da13ea3b30905937fc717d6b8cbc
callisto-CH <160159416+callisto-CH@users.noreply.github.com>
2024-12-08 12:05:46 -0500

    Add icons

---
+++ src/icons.js
@@ -0,0 +1,47 @@
+import clearDay from "./icons/clear-day.svg";
+import clearNight from "./icons/clear-night.svg";
+import cloudy from "./icons/cloudy.svg";
+import fog from "./icons/fog.svg";
+import hail from "./icons/hail.svg";
+import partlyCloudyDay from "./icons/partly-cloudy-day.svg";
+import partlyCloudyNight from "./icons/partly-cloudy-night.svg";
+import rainSnowShowersDay from "./icons/rain-snow-showers-day.svg";
+import rainSnowShowersNight from "./icons/rain-snow-showers-night.svg";
+import rainSnow from "./icons/rain-snow.svg";
+import rain from "./icons/rain.svg";
+import showersDay from "./icons/showers-day.svg";
+import showersNight from "./icons/showers-night.svg";
+import sleet from "./icons/sleet.svg";
+import snowShowersDay from "./icons/snow-showers-day.svg";
+import snowShowersNight from "./icons/snow-showers-night.svg";
+import snow from "./icons/snow.svg";
+import thunderRain from "./icons/thunder-rain.svg";
+import thunderShowersDay from "./icons/thunder-showers-day.svg";
+import thunderShowersNight from "./icons/thunder-showers-night.svg";
+import thunder from "./icons/thunder.svg";
+import wind from "./icons/wind.svg";
+
+export const icons = {
+  ["clear-day"]: clearDay,
+  ["clear-night"]: clearNight,
+  ["cloudy"]: cloudy,
+  ["fog"]: fog,
+  ["hail"]: hail,
+  ["partly-cloudy-day"]: partlyCloudyDay,
+  ["partly-cloudy-night"]: partlyCloudyNight,
+  ["rain-snow-showers-day"]: rainSnowShowersDay,
+  ["rain-snow-showers-night"]: rainSnowShowersNight,
+  ["rain-snow"]: rainSnow,
+  ["rain"]: rain,
+  ["showers-day"]: showersDay,
+  ["showers-night"]: showersNight,
+  ["sleet"]: sleet,
+  ["snow-showers-day"]: snowShowersDay,
+  ["snow-showers-night"]: snowShowersNight,
+  ["snow"]: snow,
+  ["thunder-rain"]: thunderRain,
+  ["thunder-showers-day"]: thunderShowersDay,
+  ["thunder-showers-night"]: thunderShowersNight,
+  ["thunder"]: thunder,
+  ["wind"]: wind,
+};
